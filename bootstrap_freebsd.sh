a2e83d71a5d4182c96369ce6a3ce784e8a57d0eb
Kirill Radzikhovskyy <kirillrdy@gmail.com>
2017-03-11 08:17:17 +1100

    spring cleaning

--- bootstrap_freebsd.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-pkg update
-pkg install -y go
-go run freebsd_install.go
--- install_st.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-dwm_conf_dir=`pwd`
-cd /usr/dports/x11/sterm
-make clean
-make deinstall
-make ST_CONF=$dwm_conf_dir/st_config.h install
-make clean
