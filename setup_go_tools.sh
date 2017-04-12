165a484662d04c2bcd2908de7b742eb8e1007f7c
Kirill Radzikhovskyy <kirillrdy@gmail.com>
2017-04-12 19:16:36 +1000

    go utils

---
+++ setup_go_tools.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+go get -u -v github.com/nsf/gocode
+go get -u -v github.com/rogpeppe/godef
+go get -u -v golang.org/x/tools/cmd/guru
+go get -u -v golang.org/x/tools/cmd/gorename
+go get -u -v golang.org/x/tools/cmd/goimports
+go get -u -v github.com/alecthomas/gometalinter
+gometalinter --install --update
