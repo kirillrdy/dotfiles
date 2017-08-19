8acee6bf1f3e4316e705131a017f5b182816bc28
Kirill Radzikhovskyy <kirillrdy@gmail.com>
2017-08-20 07:44:27 +1000

    Some packages for freebsd

---
+++ freebsd_install.sh
@@ -0,0 +1,23 @@
+pkg install -y \
+	beadm \
+	emacs25 \
+	firefox \
+	gdm \
+	gnome3-lite \
+	sakura \
+	screen \
+	vim \
+	xorg \
+  chromium \
+  cmake \
+  git \
+  libreoffice \
+  portmaster \
+  tig \
+  eog \
+  rsync \
+  file-roller \
+  gnome-system-monitor \
+  ruby-build \
+  the_silver_searcher \
+  virtualbox-ose
