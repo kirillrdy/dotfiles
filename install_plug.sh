3bb800c042f85f9e441d678a882c53558d518363
Kirill Radzikhovskyy <kirillrdy@gmail.com>
2017-09-26 14:41:23 +1000

    nvim

--- install_plug.sh
+++ install_plug.sh
@@ -1,2 +1,5 @@
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
