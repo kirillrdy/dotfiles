2c42d40c5d81c9d8cb1cdfccd59f6f97c7715727
Kirill Radzikhovskyy <kirillrdy@gmail.com>
2017-11-16 14:20:31 +1100

    Lets not confuse us, vim is dead

--- install.sh
+++ install.sh
@@ -2,7 +2,6 @@
 
 cp -v dot.gitconfig ~/.gitconfig
 cp -v dot.tigrc ~/.tigrc
-cp -v dot.vimrc ~/.vimrc
 mkdir -p ~/.config/nvim
 cp -v dot.vimrc ~/.config/nvim/init.vim
 cp -v dot.screenrc ~/.screenrc
