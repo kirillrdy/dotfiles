99077fe2168041e52fc1e0777bd6fb0baaee1202
Kirill Radzikhovskyy <kirillrdy@gmail.com>
2018-06-17 09:52:41 +0900

    Fixing icon for firefox

--- install.sh
+++ install.sh
@@ -5,3 +5,5 @@ cp -v dot.tigrc ~/.tigrc
 mkdir -p ~/.config/nvim
 cp -v dot.vimrc ~/.config/nvim/init.vim
 cp -v dot.screenrc ~/.screenrc
+mkdir -p ~/.local/share/icons/hicolor/scalable/apps
+cp -v firefox.png ~/.local/share/icons/hicolor/scalable/apps/
