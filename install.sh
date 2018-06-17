aa66b1869f61b7cf7b91d232259f63635398f57e
Kirill Radzikhovskyy <kirillrdy@gmail.com>
2018-06-17 09:52:41 +0900

    Fixing icon for firefox

--- install.sh
+++ install.sh
@@ -14,3 +14,4 @@ cp -v org.daa.NeovimGtk.desktop ~/.local/share/applications/
 cp -v Xdefaults ~/.Xdefaults
 cp -v git-completion.bash ~/.git-completion.bash
 cp -v git-completion.tcsh ~/.git-completion.tcsh
+cp -v firefox.png ~/.local/share/icons/hicolor/scalable/apps/
