59c251abf622235980eb80611ca10361f1d9316f
Kirill Radzikhovskyy <kirillrdy@gmail.com>
2018-06-17 10:23:41 +0900

    Moved icons into own dir

--- install.sh
+++ install.sh
@@ -7,7 +7,5 @@ cp -v dot.vimrc ~/.config/nvim/init.vim
 cp -v dot.screenrc ~/.screenrc
 mkdir -p ~/.local/share/icons/hicolor/scalable/apps
 mkdir -p ~/.local/share/applications
-cp -v firefox.png ~/.local/share/icons/hicolor/scalable/apps/
-cp -v thunderbird.png ~/.local/share/icons/hicolor/scalable/apps/
-cp -v org.daa.NeovimGtk.svg ~/.local/share/icons/hicolor/scalable/apps/
+cp -v icons/* ~/.local/share/icons/hicolor/scalable/apps/
 cp -v org.daa.NeovimGtk.desktop ~/.local/share/applications/
