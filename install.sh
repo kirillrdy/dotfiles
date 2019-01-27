b83e8c578002fc7ad187487c1e34b413acdbf40f
Kirill Radzikhovskyy <kirillrdy@gmail.com>
2019-01-27 11:16:28 +1100

    Trying to restore vim

--- install.sh
+++ install.sh
@@ -5,6 +5,7 @@ cp -v dot.tigrc ~/.tigrc
 mkdir -p ~/.config/nvim
 cp -v init.vim ~/.config/nvim/init.vim
 cp -v ginit.vim ~/.config/nvim/ginit.vim
+cp -v init.vim ~/.vimrc
 cp -v dot.screenrc ~/.screenrc
 mkdir -p ~/.local/share/icons/hicolor/scalable/apps
 mkdir -p ~/.local/share/applications
