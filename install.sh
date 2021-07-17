70208a0a3a50d5f0084b0a44db149b82c688a036
Kirill Radzikhovskyy <kirillrdy@gmail.com>
2021-07-17 10:42:13 +1000

    Less stuff

--- install.sh
+++ install.sh
@@ -7,11 +7,4 @@ cp -v init.vim ~/.config/nvim/init.vim
 cp -v ginit.vim ~/.config/nvim/ginit.vim
 cp -v init.vim ~/.vimrc
 cp -v dot.screenrc ~/.screenrc
-mkdir -p ~/.local/share/icons/hicolor/scalable/apps
-mkdir -p ~/.local/share/applications
-cp -v icons/* ~/.local/share/icons/hicolor/scalable/apps/
-cp -v org.daa.NeovimGtk.desktop ~/.local/share/applications/
 cp -v Xdefaults ~/.Xdefaults
-cp -v git-completion.bash ~/.git-completion.bash
-cp -v git-completion.tcsh ~/.git-completion.tcsh
-cp -v firefox.png ~/.local/share/icons/hicolor/scalable/apps/
