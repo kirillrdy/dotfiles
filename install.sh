02da72de310cd8a51e6ee302d4f89b59bab5abcd
Kirill Radzikhovskyy <kirillrdy@gmail.com>
2021-07-25 16:03:40 +1000

    This setup is no longer vim compatible

--- install.sh
+++ install.sh
@@ -5,6 +5,5 @@ cp -v dot.tigrc ~/.tigrc
 mkdir -p ~/.config/nvim
 cp -v init.vim ~/.config/nvim/init.vim
 cp -v ginit.vim ~/.config/nvim/ginit.vim
-cp -v init.vim ~/.vimrc
 cp -v dot.screenrc ~/.screenrc
 cp -v Xdefaults ~/.Xdefaults
