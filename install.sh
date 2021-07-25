8a1402a9f3484566fe09abe4b0be5cd63f96aa36
Kirill Radzikhovskyy <kirillrdy@gmail.com>
2021-07-26 08:45:10 +1000

    Less stuff again

--- install.sh
+++ install.sh
@@ -4,6 +4,4 @@ cp -v dot.gitconfig ~/.gitconfig
 cp -v dot.tigrc ~/.tigrc
 mkdir -p ~/.config/nvim
 cp -v init.vim ~/.config/nvim/init.vim
-cp -v ginit.vim ~/.config/nvim/ginit.vim
-cp -v dot.screenrc ~/.screenrc
 cp -v Xdefaults ~/.Xdefaults
