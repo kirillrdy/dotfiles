a79794433738f7f5e09c340b02be16f2f74f9784
Kirill Radzikhovskyy <kirillrdy@gmail.com>
2021-08-23 20:32:35 +1000

    Revert "Less stuff again"
    This reverts commit 8a1402a9f3484566fe09abe4b0be5cd63f96aa36.

--- install.sh
+++ install.sh
@@ -4,4 +4,6 @@ cp -v dot.gitconfig ~/.gitconfig
 cp -v dot.tigrc ~/.tigrc
 mkdir -p ~/.config/nvim
 cp -v init.vim ~/.config/nvim/init.vim
+cp -v ginit.vim ~/.config/nvim/ginit.vim
+cp -v dot.screenrc ~/.screenrc
 cp -v Xdefaults ~/.Xdefaults
