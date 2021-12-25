91776f8cf075c78438d511cdb723502586eed836
Kirill Radzikhovskyy <kirillrdy@gmail.com>
2021-12-26 10:43:02 +1100

    Using master !

--- flake.nix
+++ flake.nix
@@ -1,6 +1,8 @@
 {
   description = "A very basic flake";
 
+  inputs.nixpkgs.url = "github:nixos/nixpkgs";
+
   outputs = { self, nixpkgs }: {
 
     packages.x86_64-linux.hello = nixpkgs.legacyPackages.x86_64-linux.hello;
