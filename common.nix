12f8cc1837af82ed9f0f108032bde2f85212dd2e
Kirill Radzikhovskyy <kirillrdy@gmail.com>
2021-12-26 09:49:08 +1100

    New unified config

---
+++ common.nix
@@ -0,0 +1,67 @@
+{ config
+, pkgs
+, hostName
+, enableNvidia ? false
+, ...
+}:
+
+{
+  imports =
+    [
+      /etc/nixos/hardware-configuration.nix
+    ];
+  nix = {
+    extraOptions = ''
+      experimental-features = nix-command flakes
+    '';
+  };
+  nixpkgs.config.allowUnfree = true;
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+  boot.kernelPackages = pkgs.linuxPackages_latest;
+  boot.zfs.enableUnstable = true;
+
+  networking.hostId = "00000000";
+  networking.hostName = hostName;
+  time.timeZone = "Australia/Melbourne";
+
+  services.logind.extraConfig = ''
+    RuntimeDirectorySize=10G
+  '';
+
+  i18n.defaultLocale = "en_AU.UTF-8";
+  services.xserver.enable = true;
+  services.xserver.desktopManager.gnome.enable = true;
+  #services.xserver.displayManager.gdm.enable = true;
+  services.xserver.displayManager.autoLogin.enable = true;
+  services.xserver.displayManager.autoLogin.user = "kirillvr";
+  services.xserver.xkbOptions = "caps:none";
+  services.xserver.videoDrivers = if enableNvidia then [ "nvidia" ] else [ "modesetting" ];
+
+  environment.systemPackages = with pkgs; [
+    chromium
+    emacs
+    firefox
+    git
+    neovim
+    neovim-qt
+    peek
+    ripgrep
+    rnix-lsp
+    slack
+    tig
+    xclip
+  ];
+  users.users.kirillvr = {
+    isNormalUser = true;
+    extraGroups = [ "wheel" "docker" "vboxusers" ];
+  };
+  virtualisation.virtualbox.host.enable = true;
+  virtualisation.docker.enable = true;
+  virtualisation.docker.storageDriver = "zfs";
+  virtualisation.docker.enableNvidia = enableNvidia;
+  hardware.opengl.driSupport32Bit = enableNvidia;
+
+  networking.firewall.enable = false;
+  system.stateVersion = "21.05"; # Did you read the comment?
+}
--- osaka.nix
+++ osaka.nix
@@ -1,63 +1,6 @@
 { config, pkgs, ... }:
-
-{
-  imports =
-    [
-      /etc/nixos/hardware-configuration.nix
-    ];
-  nix = {
-    extraOptions = ''
-      experimental-features = nix-command flakes
-    '';
-  };
-  nixpkgs.config.allowUnfree = true;
-  boot.loader.systemd-boot.enable = true;
-  boot.loader.efi.canTouchEfiVariables = true;
-  boot.kernelPackages = pkgs.linuxPackages_latest;
-  boot.zfs.enableUnstable = true;
-
-  networking.hostId = "00000000";
-  networking.hostName = "osaka";
-  time.timeZone = "Australia/Melbourne";
-
-  services.logind.extraConfig = ''
-    RuntimeDirectorySize=20G
-  '';
-
-  networking.useDHCP = false;
-  networking.interfaces.wlp0s20f3.useDHCP = true;
-
-  i18n.defaultLocale = "en_AU.UTF-8";
-  services.xserver.enable = true;
-  services.xserver.desktopManager.gnome.enable = true;
-  #services.xserver.displayManager.gdm.enable = true;
-  services.xserver.displayManager.autoLogin.enable = true;
-  services.xserver.displayManager.autoLogin.user = "kirillvr";
-  services.xserver.xkbOptions = "caps:none";
-
-  environment.systemPackages = with pkgs; [
-    chromium
-    emacs
-    firefox
-    git
-    neovim
-    neovim-qt
-    peek
-    ripgrep
-    rnix-lsp
-    slack
-    tig
-    xclip
-  ];
-  users.users.kirillvr = {
-    isNormalUser = true;
-    extraGroups = [ "wheel" "docker" "vboxusers" ];
-  };
-  virtualisation.virtualbox.host.enable = true;
-  virtualisation.docker.enable = true;
-  virtualisation.docker.storageDriver = "zfs";
-
-  networking.firewall.enable = false;
-  system.stateVersion = "21.05"; # Did you read the comment?
+import ./common.nix {
+  hostName = "osaka";
+  inherit config pkgs;
 }
 
