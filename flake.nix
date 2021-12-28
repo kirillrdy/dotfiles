5f01ce77ede8bb4226a342fc7e016f3949946c0d
Kirill Radzikhovskyy <kirillrdy@gmail.com>
2021-12-28 17:38:52 +1100

    Even less code !

--- flake.nix
+++ flake.nix
@@ -3,27 +3,27 @@
   inputs.nixpkgs.url = "github:nixos/nixpkgs";
   inputs.awsebcli.url = "github:kirillrdy/nixpkgs/awsebcli";
   outputs = { self, nixpkgs, awsebcli }:
-    let
-      shinseikai = { config, pkgs, lib, ... }:
-        import ./common.nix {
-          inherit config pkgs lib awsebcli;
-          hostName = "shinseikai";
-          enableNvidia = true;
-        };
-      osaka = { config, pkgs, lib, ... }:
-        import ./common.nix {
-          inherit config pkgs lib awsebcli;
-          hostName = "osaka";
-        };
-    in
     {
       nixosConfigurations.osaka = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
-        modules = [ osaka ];
+        modules = [
+          ({ config, pkgs, lib, ... }:
+            import ./common.nix {
+              inherit config pkgs lib awsebcli;
+              hostName = "osaka";
+            })
+        ];
       };
       nixosConfigurations.shinseikai = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
-        modules = [ shinseikai ];
+        modules = [
+          ({ config, pkgs, lib, ... }:
+            import ./common.nix {
+              inherit config pkgs lib awsebcli;
+              hostName = "shinseikai";
+              enableNvidia = true;
+            })
+        ];
       };
     };
 }
