1044a03f00853e4b71208067dba75686bf806107
Kirill Radzikhovskyy <kirillrdy@gmail.com>
2021-12-30 07:31:33 +1100

    Refactored everything to be inline

--- flake.nix
+++ flake.nix
@@ -9,8 +9,88 @@
           simplesystem = hostName: enableNvidia: {
             system = "x86_64-linux";
             modules = [
-              ({ pkgs, lib, ... }:
-                import ./common.nix { inherit pkgs lib awsebcli hostName enableNvidia; })
+              ({ pkgs
+               , lib
+               , modulesPath
+               , ...
+               }:
+                {
+                  imports =
+                    [ (modulesPath + "/installer/scan/not-detected.nix") ];
+                  boot.initrd.availableKernelModules = [ "nvme" ];
+                  fileSystems."/" =
+                    {
+                      device = "zroot/root";
+                      fsType = "zfs";
+                    };
+                  fileSystems."/boot" =
+                    {
+                      device = "/dev/nvme0n1p3";
+                      fsType = "vfat";
+                    };
+                  swapDevices =
+                    [{ device = "/dev/nvme0n1p2"; }];
+
+                  nix = {
+                    extraOptions = ''
+                      experimental-features = nix-command flakes
+                    '';
+                  };
+
+                  # TODO maybe not on desktop, check default value
+                  powerManagement.cpuFreqGovernor = lib.mkDefault "powersave";
+
+                  nixpkgs.config.allowUnfree = true;
+                  boot.loader.systemd-boot.enable = true;
+                  boot.loader.efi.canTouchEfiVariables = true;
+                  boot.kernelPackages = pkgs.linuxPackages_latest;
+                  boot.zfs.enableUnstable = true;
+
+                  networking.hostId = "00000000";
+                  networking.hostName = hostName;
+                  time.timeZone = "Australia/Melbourne";
+
+                  services.logind.extraConfig = ''
+                    RuntimeDirectorySize=10G
+                  '';
+
+                  i18n.defaultLocale = "en_AU.UTF-8";
+                  services.xserver.enable = true;
+                  services.xserver.desktopManager.gnome.enable = true;
+                  #services.xserver.displayManager.gdm.enable = true;
+                  services.xserver.displayManager.autoLogin.enable = true;
+                  services.xserver.displayManager.autoLogin.user = "kirillvr";
+                  services.xserver.xkbOptions = "caps:none";
+                  services.xserver.videoDrivers = if enableNvidia then [ "nvidia" ] else [ "modesetting" ];
+
+                  environment.systemPackages = with pkgs; [
+                    awsebcli.legacyPackages.x86_64-linux.awsebcli
+                    chromium
+                    emacs
+                    firefox
+                    git
+                    neovim
+                    neovim-qt
+                    peek
+                    ripgrep
+                    rnix-lsp
+                    slack
+                    tig
+                    xclip
+                  ];
+                  users.users.kirillvr = {
+                    isNormalUser = true;
+                    extraGroups = [ "wheel" "docker" "vboxusers" ];
+                  };
+                  virtualisation.virtualbox.host.enable = true;
+                  virtualisation.docker.enable = true;
+                  virtualisation.docker.storageDriver = "zfs";
+                  virtualisation.docker.enableNvidia = enableNvidia;
+                  hardware.opengl.driSupport32Bit = enableNvidia;
+
+                  networking.firewall.enable = false;
+                  system.stateVersion = "21.11"; # Did you read the comment?
+                })
             ];
           };
         in
