a12f22e22b7dac2eb9b999c0940ebe8c21c9ade2
Kirill Radzikhovskyy <kirillrdy@gmail.com>
2022-01-02 15:04:00 +1100

    Some vm friendly settings

--- flake.nix
+++ flake.nix
@@ -16,7 +16,15 @@
                }:
                 {
                   imports =
-                    [ (modulesPath + "/installer/scan/not-detected.nix") ];
+                    [
+                      (modulesPath + "/installer/scan/not-detected.nix")
+                      "${nixpkgs}/nixos/modules/virtualisation/qemu-vm.nix"
+                    ];
+
+                  virtualisation.memorySize = 8000;
+                  virtualisation.cores = 8;
+                  virtualisation.diskSize = 1024 * 10;
+
                   boot.initrd.availableKernelModules = [ "nvme" ];
                   fileSystems."/" =
                     {
