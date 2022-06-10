ec31a40120f4a07ab9dc1206eb9ad8f7d163680c
Kirill Radzikhovskyy <kirillrdy@gmail.com>
2022-06-10 13:52:46 +1000

    helix welcome to the family

--- install.sh
+++ install.sh
@@ -2,7 +2,9 @@
 
 cp -v dot.gitconfig ~/.gitconfig
 cp -v dot.tigrc ~/.tigrc
+mkdir -p ~/.config/helix
 mkdir -p ~/.config/nvim
+cp -v config.toml ~/.config/helix/
 cp -v init.vim ~/.config/nvim/init.vim
 cp -v ginit.vim ~/.config/nvim/ginit.vim
 cp -v ./dot.xinitrc ~/.xinitrc
