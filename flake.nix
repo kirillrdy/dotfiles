390c1168a68c07844f98dc4d1497fcac3bbc36c7
Ran Xiao <ben.xiao@me.com>
2022-08-05 17:04:23 +1000

    add disks to dante

--- flake.nix
+++ flake.nix
@@ -87,7 +87,7 @@
                     remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
                     dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
                   };
-
+                  programs.gnome-disks.enable = true;
                   environment.systemPackages = with pkgs; [
                     awscli2
                     awsebcli
