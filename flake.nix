3c43f5055820196133c5e92ae1ff043b12425f75
Ran Xiao <ben.xiao@me.com>
2022-08-05 16:47:15 +1000

    add steam to dante

--- flake.nix
+++ flake.nix
@@ -82,7 +82,12 @@
                     enableSSHSupport = true;
                   };
 
-                  environment.variables.EDITOR = "nvim";
+                  programs.steam = {
+                    enable = true;
+                    remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
+                    dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
+                  };
+
                   environment.systemPackages = with pkgs; [
                     awscli2
                     awsebcli
