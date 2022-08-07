8fd48fa1039d18fbe0f6a6cc701f69411abdd232
Ran Xiao <ben.xiao@me.com>
2022-08-07 10:00:17 +1000

    enable gdm

--- flake.nix
+++ flake.nix
@@ -64,8 +64,8 @@
                   services.gnome.tracker-miners.enable = false;
                   services.gnome.tracker.enable = false;
                   services.xserver.desktopManager.gnome.enable = true;
+                  services.xserver.displayManager.gdm.enable = true;
                   services.xserver.displayManager.gdm.autoSuspend = !server;
-                  services.xserver.displayManager.gdm.enable = false;
                   services.xserver.enable = true;
                   services.xserver.libinput.enable = true;
                   services.xserver.videoDrivers = if enableNvidia then [ "nvidia" ] else [ "modesetting" ];
