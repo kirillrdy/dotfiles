bbd2a98e7cb2aef6a644454c99d4074c85c6ea1d
Kirill Radzikhovskyy <kirillrdy@gmail.com>
2022-10-19 15:00:35 +1100

    why does ahavi has so many options

--- flake.nix
+++ flake.nix
@@ -31,6 +31,9 @@
                   fonts.fonts = with pkgs; [ carlito dejavu_fonts ipafont kochi-substitute source-code-pro ttf_bitstream_vera ];
 
                   networking.hostId = "00000000";
+                  services.avahi.nssmdns = true;
+                  services.avahi.publish.enable = true;
+                  services.avahi.publish.addresses = true;
                   networking.hostName = hostName;
                   time.timeZone = "Australia/Melbourne";
 
