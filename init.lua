3c81b76ba5c8a8aa1297881358d5401f9133e78b
Kirill Radzikhovskyy <kirillrdy@gmail.com>
2022-12-27 07:49:05 +0900

    lsp formatting

--- init.lua
+++ init.lua
@@ -20,45 +20,45 @@ require('packer').startup(function(use)
   use 'wbthomason/packer.nvim'
 
   use {
- 	  'nvim-telescope/telescope.nvim', tag = '0.1.0',
- 	  requires = { {'nvim-lua/plenary.nvim'} }
-   }
- 
-   use({
- 	  'rose-pine/neovim',
- 	  as = 'rose-pine',
- 	  config = function()
- 		  vim.cmd('colorscheme rose-pine')
- 	  end
-   })
- 
-   use({'nvim-treesitter/nvim-treesitter', run = ':TSUpdate'})
-   use('nvim-treesitter/playground')
-   use('tpope/vim-fugitive')
- 
-   use {
- 	  'VonHeikemen/lsp-zero.nvim',
- 	  requires = {
- 		  -- LSP Support
- 		  {'neovim/nvim-lspconfig'},
- 		  {'williamboman/mason.nvim'},
- 		  {'williamboman/mason-lspconfig.nvim'},
- 
- 		  -- Autocompletion
- 		  {'hrsh7th/nvim-cmp'},
- 		  {'hrsh7th/cmp-buffer'},
- 		  {'hrsh7th/cmp-path'},
- 		  {'saadparwaiz1/cmp_luasnip'},
- 		  {'hrsh7th/cmp-nvim-lsp'},
- 		  {'hrsh7th/cmp-nvim-lua'},
- 
- 		  -- Snippets
- 		  {'L3MON4D3/LuaSnip'},
- 		  {'rafamadriz/friendly-snippets'},
- 	  }
-   }
- 
-   use("folke/zen-mode.nvim")
+    'nvim-telescope/telescope.nvim', tag = '0.1.0',
+    requires = { { 'nvim-lua/plenary.nvim' } }
+  }
+
+  use({
+    'rose-pine/neovim',
+    as = 'rose-pine',
+    config = function()
+      vim.cmd('colorscheme rose-pine')
+    end
+  })
+
+  use({ 'nvim-treesitter/nvim-treesitter', run = ':TSUpdate' })
+  use('nvim-treesitter/playground')
+  use('tpope/vim-fugitive')
+
+  use {
+    'VonHeikemen/lsp-zero.nvim',
+    requires = {
+      -- LSP Support
+      { 'neovim/nvim-lspconfig' },
+      { 'williamboman/mason.nvim' },
+      { 'williamboman/mason-lspconfig.nvim' },
+
+      -- Autocompletion
+      { 'hrsh7th/nvim-cmp' },
+      { 'hrsh7th/cmp-buffer' },
+      { 'hrsh7th/cmp-path' },
+      { 'saadparwaiz1/cmp_luasnip' },
+      { 'hrsh7th/cmp-nvim-lsp' },
+      { 'hrsh7th/cmp-nvim-lua' },
+
+      -- Snippets
+      { 'L3MON4D3/LuaSnip' },
+      { 'rafamadriz/friendly-snippets' },
+    }
+  }
+
+  use("folke/zen-mode.nvim")
 end)
 
 
@@ -66,7 +66,7 @@ end)
 local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
 vim.keymap.set('n', '<leader>s', builtin.lsp_document_symbols, {})
-vim.keymap.set('n', '<leader>e', function() builtin.diagnostics({bufnr=0}) end, {})
+vim.keymap.set('n', '<leader>e', function() builtin.diagnostics({ bufnr = 0 }) end, {})
 vim.keymap.set('n', 'gd', builtin.lsp_definitions, {})
 vim.keymap.set('n', 'gr', builtin.lsp_references, {})
 vim.keymap.set('n', 'gi', builtin.lsp_implementations, {})
@@ -75,7 +75,7 @@ vim.keymap.set('n', '<C-b>', builtin.buffers, {})
 vim.keymap.set('n', '<C-g>', builtin.grep_string, {})
 vim.keymap.set('n', 'L', builtin.live_grep, {})
 vim.keymap.set('n', '<leader>ps', function()
-	builtin.grep_string({ search = vim.fn.input("Grep > ") })
+  builtin.grep_string({ search = vim.fn.input("Grep > ") })
 end)
 
 local lsp = require('lsp-zero')
@@ -84,7 +84,7 @@ lsp.nvim_workspace()
 
 
 lsp.on_attach(function(client, bufnr)
-  local opts = {buffer = bufnr, remap = false}
+  local opts = { buffer = bufnr, remap = false }
   vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
   vim.keymap.set("n", "[d", vim.diagnostic.goto_next, opts)
   vim.keymap.set("n", "]d", vim.diagnostic.goto_prev, opts)
