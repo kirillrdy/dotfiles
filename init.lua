f5f81200e7661ac7baf2f9536af3d098aec86c3a
Kirill Radzikhovskyy <kirillrdy@gmail.com>
2022-12-27 11:52:31 +0900

    inline

--- init.lua
+++ init.lua
@@ -105,8 +105,5 @@ require 'nvim-treesitter.configs'.setup {
   ensure_installed = { "help", "nix", "lua", "rust" },
   sync_install = false,
   auto_install = true,
-  highlight = {
-    enable = true,
-    additional_vim_regex_highlighting = false,
-  },
+  highlight = { enable = true, additional_vim_regex_highlighting = false },
 }
