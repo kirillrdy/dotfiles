478c722e78b8d101bde4b1416afbc9e618508f8c
Kirill Radzikhovskyy <kirillrdy@gmail.com>
2023-01-17 08:52:23 +0900

    packer

--- install.sh
+++ install.sh
@@ -8,3 +8,5 @@ cp -v config.toml ~/.config/helix/
 cp -v init.lua ~/.config/nvim/
 cp -v ./dot.xinitrc ~/.xinitrc
 cp -v Xdefaults ~/.Xdefaults
+git clone --depth 1 https://github.com/wbthomason/packer.nvim\
+ ~/.local/share/nvim/site/pack/packer/start/packer.nvim
--- install_spacemacs.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-set -xe
-rm -rf ~/.emacs.d
-git clone https://github.com/syl20bnr/spacemacs ~/.emacs.d
