a01a5254143d862bc280c5142661f86505414dea
Kirill Radzikhovskyy <kirillrdy@gmail.com>
2023-02-24 21:47:12 +1100

    updated with nothing

--- install.sh
+++ install.sh
@@ -1,11 +1,4 @@
 #!/bin/sh
 
-cp -v dot.tigrc ~/.tigrc
-mkdir -p ~/.config/helix
-mkdir -p ~/.config/nvim
-cp -v config.toml ~/.config/helix/
-cp -v init.lua ~/.config/nvim/
 cp -v ./dot.xinitrc ~/.xinitrc
 cp -v Xdefaults ~/.Xdefaults
-git clone --depth 1 https://github.com/wbthomason/packer.nvim\
- ~/.local/share/nvim/site/pack/packer/start/packer.nvim
