54c3c86d7380e43510753f708630dba4ea366e5d
Kirill Radzikhovskyy <kirillrdy@gmail.com>
2023-03-12 17:24:54 +1100

    for now we keep microsoft-edge

--- flake.nix
+++ flake.nix
@@ -82,6 +82,7 @@
                   };
                   environment.systemPackages = with pkgs; [
                     awscli2
+                    microsoft-edge
                     awsebcli
                     evince
                     ffmpeg
