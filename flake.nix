bb5da8ca871aba40e527c773874d39b1de90b41a
Kirill Radzikhovskyy <kirillrdy@gmail.com>
2023-03-14 16:06:27 +1100

    removing the substitutes for now

--- flake.nix
+++ flake.nix
@@ -4,21 +4,8 @@
   outputs = { self, nixpkgs }:
     {
       packages.x86_64-linux.neovim = import ./neovim.nix { pkgs = import nixpkgs { system = "x86_64-linux"; }; };
-      packages.x86_64-darwin.neovim = import ./neovim.nix { pkgs = import nixpkgs { system = "x86_64-darwin"; }; };
       nixosConfigurations =
         let
-          substitute = "s3://wps-0000000000/nix?region=ap-southeast-2";
-          pkgs = import nixpkgs { system = "x86_64-linux"; };
-          updateScript = pkgs.writeScript "update" ''
-            #!/bin/sh
-
-            set -eu
-            set -f # disable globbing
-            export IFS=' '
-
-            echo "Uploading paths" $OUT_PATHS
-            exec nix copy --to "${substitute}" $OUT_PATHS
-          '';
           simplesystem = { hostName, enableNvidia ? false }: {
             system = "x86_64-linux";
             modules = [
@@ -26,20 +13,13 @@
                 {
                   imports = [ (modulesPath + "/installer/scan/not-detected.nix") ];
                   boot.binfmt.emulatedSystems = [ "aarch64-linux" ];
-
                   boot.initrd.availableKernelModules = [ "nvme" ];
                   fileSystems."/" = { device = "zroot/root"; fsType = "zfs"; };
                   fileSystems."/boot" = { device = "/dev/nvme0n1p3"; fsType = "vfat"; };
                   swapDevices = [{ device = "/dev/nvme0n1p2"; }];
-                  nix = {
-                    extraOptions = ''
-                      experimental-features = nix-command flakes
-                      post-build-hook = ${updateScript}
-                    '';
-                  };
-                  nix.settings.trusted-users = [ "root" "kirillvr" ];
-                  nix.settings.substituters = [ substitute ];
-                  nix.settings.require-sigs = false;
+                  nix.extraOptions = ''
+                    experimental-features = nix-command flakes
+                  '';
 
                   powerManagement.cpuFreqGovernor = if !enableNvidia then lib.mkDefault "powersave" else null;
 
