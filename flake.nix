ce99e320d9643772169b45d74a14e695f98d8878
Kirill Radzikhovskyy <kirillrdy@gmail.com>
2023-06-06 12:39:53 +1000

    gdm not working with latest nvidia

--- flake.nix
+++ flake.nix
@@ -48,12 +48,12 @@
                   services.gnome.tracker-miners.enable = false;
                   services.gnome.tracker.enable = false;
                   services.xserver.desktopManager.gnome.enable = true;
-                  services.xserver.displayManager.gdm.enable = true;
+                  services.xserver.displayManager.gdm.enable = !enableNvidia;
                   services.xserver.enable = true;
                   services.xserver.videoDrivers = if enableNvidia then [ "nvidia" ] else [ "modesetting" ];
                   hardware.nvidia.open = enableNvidia;
                   services.xserver.xkbOptions = "caps:none";
-                  services.tailscale.enable = false;
+                  services.tailscale.enable = true;
                   services.openssh.enable = true;
                   environment.gnome.excludePackages = [ pkgs.orca ];
                   environment.variables.EDITOR = "nvim";
