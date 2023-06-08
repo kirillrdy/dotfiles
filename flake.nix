f46cffcc503eac3bd28d859c9262f65ee7acd31f
Kirill Radzikhovskyy <kirillrdy@gmail.com>
2023-06-09 07:40:57 +1000

    6.3

--- flake.nix
+++ flake.nix
@@ -27,7 +27,7 @@
                   nixpkgs.config.allowUnfree = true;
                   boot.loader.systemd-boot.enable = true;
                   boot.loader.efi.canTouchEfiVariables = true;
-                  boot.kernelPackages = pkgs.linuxPackages_6_1;
+                  boot.kernelPackages = pkgs.linuxPackages_6_3;
 
                   fonts.enableDefaultFonts = true;
                   fonts.fonts = with pkgs; [ kochi-substitute ];
