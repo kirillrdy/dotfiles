0d21b7271d27075a95637ec354fe99808905439f
Kirill Radzikhovskyy <kirillrdy@gmail.com>
2023-11-16 11:16:56 +1100

    network manager applet

--- flake.nix
+++ flake.nix
@@ -83,6 +83,7 @@
                     brightnessctl
                     dunst
                     swww
+                    networkmanagerapplet
                     rofi-wayland
                     kitty
                     linuxPackages.cpupower
