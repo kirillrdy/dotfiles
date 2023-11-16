3d6270399c0318220c532275470de59526243fb4
Kirill Radzikhovskyy <kirillrdy@gmail.com>
2023-11-16 16:34:02 +1100

    less nesting

--- flake.nix
+++ flake.nix
@@ -1,23 +1,18 @@
 {
   description = "my computers in flakes";
   inputs.nixpkgs.url = "github:nixos/nixpkgs";
-  outputs = { self, nixpkgs }:
-    {
-      packages.x86_64-linux.neovim = import ./neovim.nix (import nixpkgs { system = "x86_64-linux"; });
-      nixosConfigurations =
-        let
-          simplesystem = import ./nixos.nix;
-        in
-        {
-          # old work machine Retired 20-10-2022
-          #tsuruhashi = nixpkgs.lib.nixosSystem (simplesystem { hostName = "tsuruhashi"; rootPool = "tsuruhashi/root"; bootDevice = "/dev/sda3"; swapDevice = "/dev/sda2"; });
-          # amd ryzen 5
-          #shinseikai = nixpkgs.lib.nixosSystem (simplesystem { hostName = "shinseikai"; enableNvidia = true; });
-          # legacy, yao: T460s
+  outputs = { self, nixpkgs }: {
+    packages.x86_64-linux.neovim = import ./neovim.nix (import nixpkgs { system = "x86_64-linux"; });
+    nixosConfigurations = {
+      # old work machine Retired 20-10-2022
+      #tsuruhashi = nixpkgs.lib.nixosSystem (simplesystem { hostName = "tsuruhashi"; rootPool = "tsuruhashi/root"; bootDevice = "/dev/sda3"; swapDevice = "/dev/sda2"; });
+      # amd ryzen 5
+      #shinseikai = nixpkgs.lib.nixosSystem (simplesystem { hostName = "shinseikai"; enableNvidia = true; });
+      # legacy, yao: T460s
 
-          # Lenovo X1 gen9
-          osaka = nixpkgs.lib.nixosSystem (simplesystem { hostName = "osaka"; buildJobs = 1; });
-          tsutenkaku = nixpkgs.lib.nixosSystem (simplesystem { hostName = "tsutenkaku"; enableNvidia = true; buildJobs = 3; });
-        };
+      # Lenovo X1 gen9
+      osaka = nixpkgs.lib.nixosSystem (import ./nixos.nix { hostName = "osaka"; buildJobs = 1; });
+      tsutenkaku = nixpkgs.lib.nixosSystem (import ./nixos.nix { hostName = "tsutenkaku"; enableNvidia = true; buildJobs = 3; });
     };
+  };
 }
