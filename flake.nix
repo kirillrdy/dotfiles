bd46281f4af1e61f5199ec81ef3aea39afff1395
Kirill Radzikhovskyy <kirillrdy@gmail.com>
2023-11-22 13:25:56 +1100

    back to master

--- flake.nix
+++ flake.nix
@@ -1,6 +1,6 @@
 {
   description = "my computers in flakes";
-  inputs.nixpkgs.url = "github:nixos/nixpkgs/release-23.11";
+  inputs.nixpkgs.url = "github:nixos/nixpkgs";
   outputs = { self, nixpkgs }: {
     packages.x86_64-linux.neovim = import ./neovim.nix (import nixpkgs { system = "x86_64-linux"; });
     nixosConfigurations = {
