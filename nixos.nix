066b95ba828dfc672628fa45622e073792683abe
Kirill Radzikhovskyy <kirillrdy@gmail.com>
2024-04-08 12:40:23 +1000

    nixfmt :-(

--- nixos.nix
+++ nixos.nix
@@ -1,17 +1,43 @@
-{ hostName, enableNvidia ? false, buildJobs ? "auto" }: {
+{
+  hostName,
+  enableNvidia ? false,
+  buildJobs ? "auto",
+}:
+{
   system = "x86_64-linux";
   modules = [
-    ({ pkgs, lib, modulesPath, ... }:
+    (
+      {
+        pkgs,
+        lib,
+        modulesPath,
+        ...
+      }:
       {
         boot.binfmt.emulatedSystems = [ "aarch64-linux" ];
         boot.initrd.availableKernelModules = [ "nvme" ];
         boot.kernelPackages = pkgs.zfs.latestCompatibleLinuxPackages;
         boot.loader.efi.canTouchEfiVariables = true;
         boot.loader.systemd-boot.enable = true;
-        environment.variables = { EDITOR = "nvim"; };
-        fileSystems."/" = { device = "zroot/root"; fsType = "zfs"; };
-        fileSystems."/boot" = { device = "/dev/nvme0n1p3"; fsType = "vfat"; };
-        fonts.packages = with pkgs; [ kochi-substitute font-awesome cantarell-fonts dejavu_fonts source-code-pro source-sans ];
+        environment.variables = {
+          EDITOR = "nvim";
+        };
+        fileSystems."/" = {
+          device = "zroot/root";
+          fsType = "zfs";
+        };
+        fileSystems."/boot" = {
+          device = "/dev/nvme0n1p3";
+          fsType = "vfat";
+        };
+        fonts.packages = with pkgs; [
+          kochi-substitute
+          font-awesome
+          cantarell-fonts
+          dejavu_fonts
+          source-code-pro
+          source-sans
+        ];
         hardware.nvidia.modesetting.enable = enableNvidia;
         i18n.defaultLocale = "en_AU.UTF-8";
         imports = [ (modulesPath + "/installer/scan/not-detected.nix") ];
@@ -25,7 +51,10 @@
         nix.settings.max-jobs = buildJobs;
         #i18n.inputMethod = { enabled = "ibus"; ibus.engines = with pkgs.ibus-engines; [ mozc ]; };
         nixpkgs.config.allowUnfree = true;
-        programs.git.config = { user.name = "Kirill Radzikhovskyy"; user.email = "kirillrdy@gmail.com"; };
+        programs.git.config = {
+          user.name = "Kirill Radzikhovskyy";
+          user.email = "kirillrdy@gmail.com";
+        };
         programs.git.enable = true;
         services.avahi.enable = true;
         services.avahi.nssmdns4 = true;
@@ -42,13 +71,28 @@
         services.xserver.enable = true;
         services.xserver.xkb.options = "caps:none";
         services.xserver.videoDrivers = if enableNvidia then [ "nvidia" ] else [ "modesetting" ];
-        swapDevices = [{ device = "/dev/nvme0n1p2"; }];
+        swapDevices = [ { device = "/dev/nvme0n1p2"; } ];
         system.stateVersion = "24.11"; # I come from the future
         time.timeZone = "Australia/Melbourne";
-        users.users.haru = { isNormalUser = true; extraGroups = [ "wheel" "docker" "vboxusers" ]; };
-        users.users.kirillvr = { isNormalUser = true; extraGroups = [ "wheel" "docker" "vboxusers" ]; };
+        users.users.haru = {
+          isNormalUser = true;
+          extraGroups = [
+            "wheel"
+            "docker"
+            "vboxusers"
+          ];
+        };
+        users.users.kirillvr = {
+          isNormalUser = true;
+          extraGroups = [
+            "wheel"
+            "docker"
+            "vboxusers"
+          ];
+        };
         virtualisation.docker.enable = true;
         virtualisation.docker.storageDriver = "zfs";
+        virtualisation.docker.enableNvidia = enableNvidia;
         environment.systemPackages = with pkgs; [
           (firefox.override { cfg.speechSynthesisSupport = false; })
           (import ./neovim.nix pkgs)
@@ -82,6 +126,7 @@
           zig
           zls
         ];
-      })
+      }
+    )
   ];
 }
