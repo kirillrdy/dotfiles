c42828f809b8a9eb7482bf15ef74ebab6013dcaa
Kirill Radzikhovskyy <kirillrdy@gmail.com>
2024-08-21 07:41:13 +1000

    space f clashed with format

--- init.lua
+++ init.lua
@@ -51,7 +51,7 @@ local on_attach = function(client, bufnr)
   vim.keymap.set("n", "]d", vim.diagnostic.goto_prev, opts)
   vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, opts)
   vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts)
-  vim.keymap.set("n", "<leader>f", vim.lsp.buf.format, opts)
+  vim.keymap.set("n", "<leader>=", vim.lsp.buf.format, opts)
   vim.keymap.set("i", "<C-h>", vim.lsp.buf.signature_help, opts)
 end
 
