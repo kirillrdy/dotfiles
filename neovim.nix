4dbe7ff31fad042b4cd9f5e91f337ca1e83bd4b4
Kirill Radzikhovskyy <kirillrdy@gmail.com>
2024-10-01 08:39:16 +1000

    templ-vim is now in nixpkgs

--- neovim.nix
+++ neovim.nix
@@ -22,17 +22,7 @@ pkgs.wrapNeovim pkgs.neovim-unwrapped {
         cmp-nvim-lsp
         cmp-nvim-lua
         nerdtree
-        (pkgs.vimUtils.buildVimPlugin {
-          pname = "templ.vim";
-          version = "2023-10-23";
-          src = pkgs.fetchFromGitHub {
-            owner = "joerdav";
-            repo = "templ.vim";
-            rev = "5cc48b93a4538adca0003c4bc27af844bb16ba24";
-            hash = "sha256-YdV8ioQJ10/HEtKQy1lHB4Tg9GNKkB0ME8CV/+hlgYs=";
-          };
-          meta.homepage = "https://github.com/joerdav/templ.vim";
-        })
+        templ-vim
       ];
     };
   };
