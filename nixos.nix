19ba3a07e98bbea6f8d0c4d8e124d82ab391278e
Kirill Radzikhovskyy <kirillrdy@gmail.com>
2024-10-12 15:15:07 +1100

    updated options

--- nixos.nix
+++ nixos.nix
@@ -80,9 +80,9 @@
         services.avahi.nssmdns4 = true;
         services.avahi.publish.addresses = true;
         services.avahi.publish.enable = true;
-        services.gnome.core-utilities.enable = false;
-        services.gnome.tracker-miners.enable = false;
-        services.gnome.tracker.enable = false;
+        services.gnome.core-utilities.enable = true;
+        services.gnome.tinysparql.enable = true;
+        services.gnome.localsearch.enable = true;
         services.logind.extraConfig = "RuntimeDirectorySize=10G";
         services.openssh.enable = true;
         services.tailscale.enable = true;
