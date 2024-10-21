bf8680108d00b3dd75288408d8bdfb6723a13bd0
Kirill Radzikhovskyy <kirillrdy@gmail.com>
2024-10-21 17:28:01 +1100

    dont need overlay anymore

--- nixos.nix
+++ nixos.nix
@@ -16,9 +16,6 @@
         ...
       }:
       {
-        nixpkgs.overlays = [
-          (final: prev: { wireplumber = prev.wireplumber.override { enableDocs = false; }; })
-        ];
         nixpkgs.hostPlatform =
           if gccarch == null then
             { system = "x86_64-linux"; }
