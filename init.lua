0108b34915dd4698645c728a3fd3dcb897f93919
Kirill Radzikhovskyy <kirillrdy@gmail.com>
2025-03-31 09:40:48 +1100

    deprecated

--- init.lua
+++ init.lua
@@ -50,8 +50,6 @@ local on_attach = function(autoformat)
     end
     local opts = { buffer = bufnr, remap = false }
     vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
-    vim.keymap.set("n", "[d", vim.diagnostic.goto_next, opts)
-    vim.keymap.set("n", "]d", vim.diagnostic.goto_prev, opts)
     vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, opts)
     vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts)
     vim.keymap.set("n", "<leader>=", vim.lsp.buf.format, opts)
