a33bf75697fb277b2302d26f6ae09eedeb0878b4
Kirill Radzikhovskyy <kirillrdy@gmail.com>
2025-07-30 10:58:01 +1000

    wrapNeovimUnstable

--- neovim.nix
+++ neovim.nix
@@ -1,43 +1,37 @@
 pkgs:
-pkgs.wrapNeovim pkgs.neovim-unwrapped {
-  configure = {
-    customRC = ''
-      lua << EOF
-      ${builtins.readFile ./init.lua}
-      EOF
-      let g:rooter_patterns = ['.git']
-    '';
-    packages.myVimPackage = with pkgs.vimPlugins; {
-      start = [
-        telescope-nvim
-        plenary-nvim
-        vim-rooter
-        kanagawa-nvim
-        (nvim-treesitter.withPlugins (
-          p: with p; [
-            c
-            cpp
-            go
-            javascript
-            just
-            nix
-            python
-            ruby
-            sql
-            terraform
-            typescript
-            zig
-          ]
-        ))
-        vim-fugitive
-        nvim-lspconfig
-        nvim-cmp
-        cmp-buffer
-        cmp-path
-        cmp-nvim-lsp
-        cmp-nvim-lua
-        nerdtree
-      ];
-    };
-  };
+pkgs.wrapNeovimUnstable pkgs.neovim-unwrapped {
+  luaRcContent = ''
+    ${builtins.readFile ./init.lua}
+    -- let g:rooter_patterns = ['.git']
+  '';
+  plugins = with pkgs.vimPlugins; [
+    telescope-nvim
+    plenary-nvim
+    vim-rooter
+    kanagawa-nvim
+    (nvim-treesitter.withPlugins (
+      p: with p; [
+        c
+        cpp
+        go
+        javascript
+        just
+        nix
+        python
+        ruby
+        sql
+        terraform
+        typescript
+        zig
+      ]
+    ))
+    vim-fugitive
+    nvim-lspconfig
+    nvim-cmp
+    cmp-buffer
+    cmp-path
+    cmp-nvim-lsp
+    cmp-nvim-lua
+    nerdtree
+  ];
 }
