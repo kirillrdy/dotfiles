8588262df16ef58a511d561124a6d530572c8ff6
Kirill Radzikhovskyy <kirillrdy@gmail.com>
2025-08-17 10:42:28 +1000

    more cleansing

--- init.lua
+++ init.lua
@@ -1,19 +1,8 @@
-vim.opt.number = true
 vim.opt.relativenumber = true
 vim.opt.tabstop = 2
-vim.opt.shiftwidth = 2
 vim.opt.expandtab = true
-vim.opt.smartindent = true
 vim.opt.swapfile = false
-vim.opt.backup = false
-vim.opt.hlsearch = true
-vim.opt.incsearch = true
-vim.opt.termguicolors = true
-vim.opt.signcolumn = "yes"
-vim.opt.isfname:append("@-@")
-vim.opt.autoread = true;
 vim.opt.clipboard = "unnamedplus"
-
 vim.cmd('colorscheme kanagawa')
 
 vim.g.mapleader = " "
@@ -33,15 +22,13 @@ vim.keymap.set('n', '<leader>b', builtin.buffers, {})
 vim.keymap.set('n', '<C-g>', builtin.grep_string, {})
 vim.keymap.set('n', 'L', builtin.live_grep, {})
 vim.keymap.set('n', 'z=', builtin.spell_suggest, {})
-
-vim.diagnostic.config({ virtual_text = { current_line = true } })
-
-vim.api.nvim_create_autocmd("BufWritePre", { callback = function() vim.lsp.buf.format() end })
-
 vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
 vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, {})
 vim.keymap.set("n", "<leader>=", vim.lsp.buf.format, {})
 vim.keymap.set("i", "<C-h>", vim.lsp.buf.signature_help, {})
+
+vim.diagnostic.config({ virtual_text = { current_line = true } })
+vim.api.nvim_create_autocmd("BufWritePre", { callback = function() vim.lsp.buf.format() end })
 
 local cmp = require('cmp')
 cmp.setup({
@@ -70,13 +57,12 @@ vim.lsp.config('nil_ls', {
   }
 })
 
+local lsps = { "golangci_lint_ls", "gopls", "lua_ls", "nil_ls", "pyright", "rubocop", "ts_ls", "zls", 'superhtml' }
+for _, name in ipairs(lsps) do
+  vim.lsp.enable(name)
+end
+
 require 'nvim-treesitter.configs'.setup {
   sync_install = false,
   highlight = { enable = true, additional_vim_regex_highlighting = false },
 }
-
-local lsps = { "golangci_lint_ls", "gopls", "lua_ls", "nil_ls", "pyright", "rubocop", "ts_ls", "zls", 'superhtml' }
-
-for _, name in ipairs(lsps) do
-  vim.lsp.enable(name)
-end
