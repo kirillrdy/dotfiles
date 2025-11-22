66788c1b4878fa4579d3e87066d2693b32f02e5e
Kirill Radzikhovskyy <kirillrdy@gmail.com>
2025-11-23 07:46:00 +1100

    build(flake): use nixpkgs PR #464084

--- flake.nix
+++ flake.nix
@@ -1,6 +1,6 @@
 {
   description = "my computers in flakes";
-  inputs.nixpkgs.url = "github:nixos/nixpkgs";
+  inputs.nixpkgs.url = "github:nixos/nixpkgs?ref=pull/464084/head";
   outputs =
     { self, nixpkgs }:
     {
