6a346d7aeacb957f145418b9d3e4d165aadfc31a
Kirill Radzikhovskyy <kirillrdy@gmail.com>
2026-04-27 09:34:46 +1000

    back to lts kernel

--- nixos.nix
+++ nixos.nix
@@ -8,8 +8,7 @@
 {
   boot.binfmt.emulatedSystems = [ "aarch64-linux" ];
   boot.initrd.availableKernelModules = [ "nvme" ];
-  boot.kernelPackages = pkgs.linuxPackages_6_19;
-  boot.zfs.package = pkgs.zfs_2_4;
+  #boot.kernelPackages = pkgs.linuxPackages_6_19;
   boot.loader.efi.canTouchEfiVariables = true;
   boot.loader.systemd-boot.enable = true;
   environment.variables = {
